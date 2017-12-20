583f365a45199c41a8d3ff66ea00849753f3b0dc
RDF014 <rdf014@gmail.com>
2017-12-20 01:30:12 -0800

    added sendgrid library

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const sendEmail = require('../service/sparkPost');
+const sendSparkPost = require('../service/sparkPost');
+const sendMailgun = require('../service/mailgun');
 
 const router = express.Router();
 
 router.route('/mail')
+  // .post((req, res) => {
+  //   console.log('sendingMail');
+  //   sendSparkPost()
+  //   .then(data => res.send(data) )
+  //   .catch(err => res.status(err.statusCode).send(err) );
+  // });
   .post((req, res) => {
-    console.log('sendingMail');
-    sendEmail()
-    .then(data => res.send(data) )
-    .catch(err => res.status(err.statusCode).send(err) );
+    sendMailgun((data, err) => {
+      if (err) {
+        return res.send({err});
+      }
+      return res.send({data});
+    })
   });
 
 module.exports = router;
--- server/service/mailGun.js
+++ server/service/mailGun.js
@@ -1,18 +1,22 @@
-const mailgun = require("mailgun-js");
-const api_key = process.env.SENDGRID_API_KEY;
+// const mailgun = require('mailgun-js')
+const api_key = process.env.MAILGUN_API_KEY;
 const domain = 'raphaeldf.com';
 const mailgun = require('mailgun-js')({apiKey: api_key, domain});
 
 const data = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+  from: 'Excited User <no_reply@raphaeldf.com>',
+  to: 'rdf014@gmail.com',
   subject: 'Hello',
   text: 'Testing some Mailgun awesomness!'
 };
 
-function sendGridEmail () {
+function sendMailgun (cb) {
   mailgun.messages().send(data, (error, body) => {
+    if (error) {
+      return cb(null, error);
+    }
     console.log(body);
+    return cb(body, null);
   });
 }
 
